da6264d3d2f2928059894b112520a0d1d5dc0a5d
Ethan Water <124078937+angelshatepop@users.noreply.github.com>
2023-02-26 15:53:13 -0500

    clean up pomodoro_rs

--- pomodoro-rs/main.rs
+++ pomodoro-rs/main.rs
@@ -1,100 +1,37 @@
 #!/usr/bin/env run-cargo-script
-use std::{
-    io::{stdout, Write},
-    thread::sleep,
-    time::Duration,
-};
+use std::{io::{stdout, Write}, thread::sleep, time::Duration};
 
-pub fn focus(mins: u32) {
+pub fn pomodoro_timer(title: &str, mins: u32) {
     let mut stdout = stdout();
     let (mut seconds, mut seconds_total) = (60, mins*60);
     loop {
-        if seconds_total == 0{
+        if seconds_total == 0 {
             break;
-        }
-        else{
+        } else {
             seconds -= 1;
             seconds_total -= 1;
-            let mins = seconds_total/60;
-            if mins < 10 {
-                if seconds == 0 {
-                    stdout.write(format!("\rfocus...  {}:00", mins).as_bytes()).unwrap();
-                    stdout.flush().unwrap();
-                    seconds = 60;
-                }
-                else{
-                    stdout.write(format!("\rfocus...  {}:{}", mins, seconds).as_bytes()).unwrap();
-                    stdout.flush().unwrap();
-                }
-                sleep(Duration::from_secs(1));
-            }
-            else {
-                if seconds == 0 {
-                    stdout.write(format!("\rfocus... {}:00", mins).as_bytes()).unwrap();
-                    stdout.flush().unwrap();
-                    seconds = 60;
-                }
-                else{
-                    stdout.write(format!("\rfocus... {}:{}", mins, seconds).as_bytes()).unwrap();
-                    stdout.flush().unwrap();
-                }
-                sleep(Duration::from_secs(1));
-            }
-        }
-    }
+            let mins = seconds_total / 60;
+            let time_str = format!("\r{}... {:02}:{:02}", title, mins, seconds);
 
-}
-pub fn rest(mins: u32) {
-    let mut stdout = stdout();
-    let (mut seconds, mut seconds_total) = (60, mins*60);
-    loop {
-        if seconds_total == 0{
-            stdout.write(format!("\rdone           ").as_bytes()).unwrap();
+            stdout.write(time_str.as_bytes()).unwrap();
             stdout.flush().unwrap();
-            break;
-        }
-        else{
-            seconds -= 1;
-            seconds_total -= 1;
-            let mins = seconds_total/60;
-            if mins < 10 {
-                if seconds == 0 {
-                    stdout.write(format!("\rrest...   {}:00", mins).as_bytes()).unwrap();
-                    stdout.flush().unwrap();
-                    seconds = 60;
-                }
-                else{
-                    stdout.write(format!("\rrest...   {}:{}", mins, seconds).as_bytes()).unwrap();
-                    stdout.flush().unwrap();
-                }
-                sleep(Duration::from_secs(1));
-            }
-            else {
-                if seconds == 0 {
-                    stdout.write(format!("\rrest...  {}:00", mins).as_bytes()).unwrap();
-                    stdout.flush().unwrap();
-                    seconds = 60;
-                }
-                else{
-                    stdout.write(format!("\rrest... {}:{}", mins, seconds).as_bytes()).unwrap();
-                    stdout.flush().unwrap();
-                }
-                sleep(Duration::from_secs(1));
-            }
+            if seconds == 0 {seconds = 60};
+            sleep(Duration::from_secs(1));
         }
     }
 }
+
 pub fn pomodoro(focus_len: u32, rest_len: u32, cycles: u32) {
     let mut cycle = 0;
-
     while cycle != cycles {
-        focus(focus_len);
-        rest(rest_len);
-        cycle += 1;
+        pomodoro_timer("focus", focus_len);
+        pomodoro_timer("rest", rest_len);
         println!("  --pomodoro({}) complete", cycle );
+        cycle += 1;
     }
     println!("\npomodoro session complete!")
 }
+
 fn main () {
-    pomodoro(25, 5, 2)
+    pomodoro(25, 5, 4)
 }
